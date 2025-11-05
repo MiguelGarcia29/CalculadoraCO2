fdc539d1ba8f4d7eb5fe71b7f001d06d12e34fb5
Jesús Miguel García Bernal <miguel.garber@alum.uca.es>
2025-11-05 14:50:02 +0100

    funcional

--- js/funciones.js
+++ js/funciones.js
@@ -193,17 +193,4 @@ function resetAll() {
 
 //PORTADA 
 
-document.addEventListener("DOMContentLoaded", () => {
-  // --- PORTADA ---
   const banderaES = document.getElementById("banderaES");
-  const banderaEN = document.getElementById("banderaEN");
-
-  if (banderaES && banderaEN) {
-    banderaES.addEventListener("click", () => {
-      window.location.href = "index.html?lang=es";
-    });
-
-    banderaEN.addEventListener("click", () => {
-      window.location.href = "index.html?lang=en";
-    });
-  }});
